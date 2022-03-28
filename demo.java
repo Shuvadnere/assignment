1d38aa95afa59f70ecaefb71860747383e83532a
shuvadnere <vadnereshubh@gmail.com>
2022-03-28 10:28:26 +0530

    demo added

---
+++ demo.java
@@ -0,0 +1,12 @@
+class demo{
+
+
+
+public static void main(String arg[])
+{
+System.out.println("Hello");
+System.out.println("Name- Shubham vadnere");
+System.out.println("Batch - B");
+System.out.println("CDAC NOIDA");
+}
+}
